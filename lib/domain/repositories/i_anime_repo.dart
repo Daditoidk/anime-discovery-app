04945e67c3380e312e9ebd996a6264c61b021e27
Daditoidk <Daditoidk@users.noreply.github.com>
2025-11-13 22:07:13 -0500

    feat: add abstract anime repository

--- lib/domain/repositories/i_anime_repo.dart
+++ lib/domain/repositories/i_anime_repo.dart
@@ -1,7 +1,10 @@
+import 'package:anime_discovery_app/core/failures/failure.dart';
+import 'package:anime_discovery_app/domain/entities/anime.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class IAnimeRepository {
 
-  Future <void> fetchAnimes();
-  Future <void> searchAnimes(String query);
+  Future<Either<Failure, List<Anime>>> getPopularAnime();
 
 
 }
