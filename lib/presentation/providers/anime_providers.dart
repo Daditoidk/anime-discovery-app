18b5d7e224db2698d92fbb416572045a52d59e49
Daditoidk <Daditoidk@users.noreply.github.com>
2025-11-14 14:48:48 -0500

    feat: add anime providers

---
+++ lib/presentation/providers/anime_providers.dart
@@ -0,0 +1,29 @@
+import 'package:anime_discovery_app/core/constants/const.dart';
+import 'package:anime_discovery_app/data/datasources/kitsu_anime_remote_datasource.dart';
+import 'package:anime_discovery_app/data/repositories/anime_repo_impl.dart';
+import 'package:anime_discovery_app/domain/repositories/i_anime_repo.dart';
+import 'package:dio/dio.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'anime_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+Dio dio(Ref ref) {
+  return Dio(
+    BaseOptions(
+      baseUrl: kKitsuAPI,
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 3),
+    ),
+  );
+}
+
+@Riverpod(keepAlive: true)
+IKitsuAPIRemoteDataSource kitsuAPIRemoteDatasource(Ref ref) {
+  return KitsuAPIRemoteDataSourceImpl(ref.watch(dioProvider));
+}
+
+@Riverpod(keepAlive: true)
+IAnimeRepository animeRepository(Ref ref) {
+  return AnimeRepositoryImpl(ref.watch(kitsuAPIRemoteDatasourceProvider));
+}
