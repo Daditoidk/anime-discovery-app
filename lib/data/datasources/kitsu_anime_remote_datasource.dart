cd32a96a0ac95964ac9833aea63c88d6e78061a2
Daditoidk <Daditoidk@users.noreply.github.com>
2025-11-14 13:55:40 -0500

    feat: add anime repo implementation, fix: add type exception in kitsu anime datasource

--- lib/data/datasources/kitsu_anime_remote_datasource.dart
+++ lib/data/datasources/kitsu_anime_remote_datasource.dart
@@ -2,11 +2,11 @@ import 'package:anime_discovery_app/core/constants/const.dart';
 import 'package:anime_discovery_app/data/models/anime_dto.dart';
 import 'package:dio/dio.dart';
 
-abstract class KitsuAPIRemoteDataSource {
+abstract class IKitsuAPIRemoteDataSource {
   Future<List<AnimeDto>> getPopularAnime();
 }
 
-class KitsuAPIRemoteDataSourceImpl implements KitsuAPIRemoteDataSource {
+class KitsuAPIRemoteDataSourceImpl implements IKitsuAPIRemoteDataSource {
   final Dio dio;
 
   KitsuAPIRemoteDataSourceImpl(this.dio);
@@ -18,7 +18,16 @@ class KitsuAPIRemoteDataSourceImpl implements KitsuAPIRemoteDataSource {
         kPopularAnimeEndpoint,
         queryParameters: {'sort': '-user_count'},
       );
+
+      final type = response.data['type'];
+      if (type != kAnimeType) {
+        throw Exception(
+          'Fetched incorrected data it should be type: $kAnimeType',
+        );
+      }
+
       final List<dynamic> data = response.data['data'];
+      
       return data.map((json) => AnimeDto.fromJson(json)).toList();
     } catch (e) {
       throw Exception('Failed to fetch popular anime: $e');
---
+++ lib/data/repositories/anime_repo_impl.dart
@@ -0,0 +1,27 @@
+import 'package:anime_discovery_app/core/extensions/mappers.dart';
+import 'package:anime_discovery_app/core/failures/failure.dart';
+import 'package:anime_discovery_app/data/datasources/kitsu_anime_remote_datasource.dart';
+import 'package:anime_discovery_app/domain/entities/anime.dart';
+import 'package:anime_discovery_app/domain/repositories/i_anime_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class AnimeRepositoryImpl implements IAnimeRepository {
+  final IKitsuAPIRemoteDataSource api;
+
+  AnimeRepositoryImpl(this.api);
+
+  @override
+  Future<Either<Failure, List<Anime>>> getPopularAnime() async {
+    try {
+      final popularAnimeDtos = await api.getPopularAnime();
+
+      final popularAnimeEntities = popularAnimeDtos
+          .map((dto) => dto.toEntity())
+          .toList();
+
+      return Right(popularAnimeEntities);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+}
