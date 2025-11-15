f5892236125ca08fcdc8885b751aa47b0e51d212
Daditoidk <Daditoidk@users.noreply.github.com>
2025-11-15 13:07:51 -0500

    refactor: change ListView for SliverList in the popular anime list

--- lib/presentation/pages/popular_anime_list_page.dart
+++ lib/presentation/pages/popular_anime_list_page.dart
@@ -14,51 +14,61 @@ class PopularAnimeListPage extends ConsumerWidget {
     final listAsync = ref.watch(popularAnimeListProvider);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Text('Popular Anime List'),
-            ),
-            Flexible(
-              child: listAsync.when(
-                data: (List<Anime> data) {
-                  return ListView.builder(
-                    itemCount: data.length,
-                    itemBuilder: (context, index) {
+        child: listAsync.when(
+          data: (List<Anime> data) {
+            return RefreshIndicator(
+              onRefresh: () async {
+                ref.invalidate(popularAnimeListProvider);
+                await ref.read(popularAnimeListProvider.notifier).refresh();
+              },
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    floating: true,
+                    pinned: true,
+                    expandedHeight: 120,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    flexibleSpace: FlexibleSpaceBar(
+                      expandedTitleScale: 1.8,
+                      title: const Text('Popular Anime'),
+                      titlePadding: EdgeInsets.symmetric(vertical: 16),
+                      centerTitle: true,
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
                       final currentAnime = data[index];
                       return AnimeTile(
                         key: ValueKey('anime_tile_${currentAnime.id}'),
                         anime: currentAnime,
                       );
-                    },
-                  );
-                },
-                error: (failure, stackTrace) {
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.error, size: 48, color: Colors.red),
-                        const SizedBox(height: 16),
-                        Text((failure as Failure).message ?? ''),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: () => ref
-                              .read(popularAnimeListProvider.notifier)
-                              .refresh(),
-                          child: const Text('Retry'),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                loading: () {
-                  return Center(child: CircularProgressIndicator());
-                },
+                    }, childCount: data.length),
+                  ),
+                ],
               ),
-            ),
-          ],
+            );
+          },
+          error: (failure, stackTrace) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.error, size: 48, color: Colors.red),
+                  const SizedBox(height: 16),
+                  Text((failure as Failure).message ?? ''),
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () =>
+                        ref.read(popularAnimeListProvider.notifier).refresh(),
+                    child: const Text('Retry'),
+                  ),
+                ],
+              ),
+            );
+          },
+          loading: () {
+            return Center(child: CircularProgressIndicator());
+          },
         ),
       ),
     );
